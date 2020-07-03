dc56380b07bf27783f5a4f09050a591af2323592
Edwin Mantilla <emantillas@psl.com.co>
2020-07-03 12:01:09 -0500

    Leccion 116. Connect

--- src/App.js
+++ src/App.js
@@ -17,8 +17,11 @@ import { setCity } from './actions'
 >>>>>>> 859d92c... 114. Mejora a action creator
 =======
 import { setCity } from './actions';
+<<<<<<< HEAD
 import { store } from './store';
 >>>>>>> 3cae83a... 115. Refactorización de Store y vinculación de Provider
+=======
+>>>>>>> 0546f79... Leccion 116. Connect
 
 const cities = [
   'Bogota,co',
@@ -45,6 +48,7 @@ class App extends Component {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     this.props.setCity(city);
 =======
     const action = {
@@ -56,6 +60,9 @@ class App extends Component {
 =======
     store.dispatch(setCity(city));
 >>>>>>> b3403fc... 113. Generación de action creator
+=======
+    this.props.setCity(city);
+>>>>>>> 0546f79... Leccion 116. Connect
   }
 
   render() {
